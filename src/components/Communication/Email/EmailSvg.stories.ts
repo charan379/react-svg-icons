392e6adae0db19f36c64a1c6c71a6d90c638eb3e
charan379 <37008444+charan379@users.noreply.github.com>
2023-09-22 21:42:07 +0530

    group under communication icons

---
+++ src/components/Communication/Email/EmailSvg.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { EmailSvg } from "./EmailSvg"
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+    title: 'React Svg Icons/Communication/EmailSvg',
+    component: EmailSvg,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+        layout: 'centered',
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        // size prop
+        size: {
+            control: "number",
+            description: "Size of icon to be rendered",
+            table: {
+                type: { summary: 'number' },
+                defaultValue: { summary: 55 },
+            },
+        },
+    },
+} satisfies Meta<typeof EmailSvg>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const EmailSvgIcon: Story = {
+    args: {
+        size: 55,
+    },
+};
