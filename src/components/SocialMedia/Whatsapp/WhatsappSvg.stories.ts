91ac83fe420683a64babed666a49d1f0596eca69
charan379 <37008444+charan379@users.noreply.github.com>
2023-09-22 23:01:04 +0530

    props updated

--- src/components/SocialMedia/Whatsapp/WhatsappSvg.stories.ts
+++ src/components/SocialMedia/Whatsapp/WhatsappSvg.stories.ts
@@ -15,13 +15,50 @@ const meta = {
     argTypes: {
         // size prop
         size: {
-            control: { type: 'number', min:24, max:1024, step: 2 },
+            control: { type: 'number', min: 24, max: 1024, step: 2 },
             description: "Size of icon to be rendered",
             table: {
                 type: { summary: 'number' },
                 defaultValue: { summary: 55 },
             },
         },
+        // outerBorderColor prop
+        outerBorderColor: {
+            control: { type: 'color', presetColors: [], },
+            description: "Color of outer border",
+            table: {
+                type: { summary: 'color' },
+                defaultValue: { summary: "#cfd8dc" },
+            },
+        },
+        // innerBorderColor prop
+        innerBorderColor: {
+            control: { type: 'color', presetColors: [], },
+            description: "Color of inner border",
+            table: {
+                type: { summary: 'color' },
+                defaultValue: { summary: "#fff" },
+            },
+        },
+        // aroundHandsetColor prop
+        aroundHandsetColor: {
+            control: { type: 'color', presetColors: [], },
+            description: "Color of around handset",
+            table: {
+                type: { summary: 'color' },
+                defaultValue: { summary: "#40c351" },
+            },
+        },
+        // handsetColor prop
+        handsetColor: {
+            control: { type: 'color', presetColors: [], },
+            description: "Color of  handset",
+            table: {
+                type: { summary: 'color' },
+                defaultValue: { summary: "#fff" },
+            },
+        }
+
     },
 } satisfies Meta<typeof WhatsappSvg>;
 
@@ -29,8 +66,22 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const WhatsappSvgIcon: Story = {
+export const Icon1: Story = {
     args: {
         size: 55,
+        outerBorderColor: "#cfd8dc",
+        innerBorderColor: "#fff",
+        aroundHandsetColor: "#40c351",
+        handsetColor: "#fff",
+    },
+};
+
+export const Icon2: Story = {
+    args: {
+        size: 55,
+        outerBorderColor: "#cfd8dc",
+        innerBorderColor: "#43C354",
+        aroundHandsetColor: "#F4F7F7",
+        handsetColor: "#43C354",
     },
 };
