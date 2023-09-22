646aa6f5509347edb3e8a1685ada2cecd20b5cc0
charan379 <37008444+charan379@users.noreply.github.com>
2023-09-22 21:44:01 +0530

    sort stories in alphabatical order

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,14 @@ import type { Preview } from "@storybook/react";
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    options: {
+      storySort: {
+        method: 'alphabetical',
+        includeNames: true,
+        order: [],
+        locales: 'en-US',
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
