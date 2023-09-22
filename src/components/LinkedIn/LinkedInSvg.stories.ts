610e13a189bf781b0b3c9d8036fa4544055df41f
Charanteja <37008444+charan379@users.noreply.github.com>
2023-09-22 10:45:50 +0000

    added LinedInSvg stories

---
+++ src/components/LinkedIn/LinkedInSvg.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { LinkedInSvg } from "./LinkedInSvg"
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+    title: 'React-Svg-Icons/LinkedInSvg',
+    component: LinkedInSvg,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+        layout: 'centered',
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        // size prop
+        size: {
+            control: "number",
+            description: "Size of icon to be rendered",
+            table: {
+                type: { summary: 'number' },
+                defaultValue: { summary: 55 },
+            },
+        },
+    },
+} satisfies Meta<typeof LinkedInSvg>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const LinkedInSvgIcon: Story = {
+    args: {
+        size: 55,
+    },
+};
