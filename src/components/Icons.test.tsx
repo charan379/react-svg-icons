4f6c14159b22e95bc9b1f57ae50218751426675c
charan379 <37008444+charan379@users.noreply.github.com>
2023-09-22 21:43:17 +0530

    changing imports according to folder structure changes

--- src/components/Icons.test.tsx
+++ src/components/Icons.test.tsx
@@ -1,54 +1,54 @@
-
-import { render } from "@testing-library/react"
+import { render } from "@testing-library/react";
 import React from "react";
-import { EmailSvg } from "./Email";
-import { GithubSvg } from "./Github";
-import { InstagramSvg } from "./Instagram";
-import { LinkedInSvg } from "./LinkedIn";
-import { TwitterSvg } from "./Twitter/TwitterSvg";
-import { WhatsappSvg } from "./Whatsapp";
-
+import {
+  GithubSvg,
+  InstagramSvg,
+  LinkedInSvg,
+  TwitterSvg,
+  WhatsappSvg,
+} from "./SocialMedia";
+import { EmailSvg } from "./Communication";
 
 describe("Svg Icons Render Test", () => {
-    // email test
-    test("Renders Email Svg icon component with size 40", () => {
-        render(<EmailSvg size={40} />);
-    });
+  // email test
+  test("Renders Email Svg icon component with size 40", () => {
+    render(<EmailSvg size={40} />);
+  });
 
-    test("Renders Email Svg icon component with default size", () => {
-        render(<EmailSvg />);
-    });
+  test("Renders Email Svg icon component with default size", () => {
+    render(<EmailSvg />);
+  });
 
-    // github test
-    test("Renders Github Svg icon component with size 40", () => {
-        render(<GithubSvg size={40} />);
-    });
+  // github test
+  test("Renders Github Svg icon component with size 40", () => {
+    render(<GithubSvg size={40} />);
+  });
 
-    test("Renders Github Svg icon component with default size", () => {
-        render(<GithubSvg />);
-    });
+  test("Renders Github Svg icon component with default size", () => {
+    render(<GithubSvg />);
+  });
 
-    // Instagram tests
-    test("Renders Instagram Svg icon component with size 40", () => {
-        render(<InstagramSvg size={40} />);
-    });
+  // Instagram tests
+  test("Renders Instagram Svg icon component with size 40", () => {
+    render(<InstagramSvg size={40} />);
+  });
 
-    test("Renders Instagram Svg icon component with default size", () => {
-        render(<InstagramSvg />);
-    });
+  test("Renders Instagram Svg icon component with default size", () => {
+    render(<InstagramSvg />);
+  });
 
-    // Linked In tests
-    test("Renders LinkedIn Svg icon component", () => {
-        render(<LinkedInSvg size={40} />);
-    });
+  // Linked In tests
+  test("Renders LinkedIn Svg icon component", () => {
+    render(<LinkedInSvg size={40} />);
+  });
 
-    // Twitter tests
-    test("Renders Twitter Svg icon component", () => {
-        render(<TwitterSvg size={40} />);
-    });
+  // Twitter tests
+  test("Renders Twitter Svg icon component", () => {
+    render(<TwitterSvg size={40} />);
+  });
 
-    // Whatsapp tests
-    test("Renders Whatsapp Svg icon component", () => {
-        render(<WhatsappSvg size={40} />);
-    });
+  // Whatsapp tests
+  test("Renders Whatsapp Svg icon component", () => {
+    render(<WhatsappSvg size={40} />);
+  });
 });
