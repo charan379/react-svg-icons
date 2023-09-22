7e2a60837fed6ead6f84cfc559074608fb99ee83
Charanteja <37008444+charan379@users.noreply.github.com>
2023-09-22 06:07:01 +0000

    added storybook

---
+++ src/components/Github/GithubSvg.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { GithubSvg } from "./GithubSvg"
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+    title: 'React-Svg-Icons/GithubSvg',
+    component: GithubSvg,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+        layout: 'centered',
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        // backgroundColor: { control: 'color' },
+        size: { control: 'number' },
+    },
+} satisfies Meta<typeof GithubSvg>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const GithubSvgIcon: Story = {
+    args: {
+        size: 20,
+    },
+};
