22bf68efd4700332f8a1f4228682e4c27ef4a6cd
charan379 <37008444+charan379@users.noreply.github.com>
2023-09-24 19:49:38 +0530

    added linkedin svg

--- src/components/SocialMedia/LinkedIn/LinkedInSvg.tsx
+++ src/components/SocialMedia/LinkedIn/LinkedInSvg.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 import { SvgProps } from "../..";
 
-export const LinkedInSvg: React.FC<SvgProps> = (props) => {
-    const { size = 55 } = props;
-    return (
-        <>
-            {/* LinkedIn Svg */}
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width={`${size}px`}
-                height={`${size}px`}
-            >
-                <path fill="none" d="M0 0h24v24H0z" />
-                <path
-                    d="M6.94 5a2 2 0 1 1-4-.002 2 2 0 0 1 4 .002zM7 8.48H3V21h4V8.48zm6.32 0H9.34V21h3.94v-6.57c0-3.66 4.77-4 4.77 0V21H22v-7.93c0-6.17-7.06-5.94-8.72-2.91l.04-1.68z"
-                    fill="#0a66c2"
-                />
-            </svg>
-        </>
-    );
+export interface LinkedInSvgProps extends SvgProps {
+  color?: string;
+}
+
+export const LinkedInSvg: React.FC<LinkedInSvgProps> = (props) => {
+  const { size = 55, color = "#0288d1" } = props;
+  return (
+    <>
+      {/* LinkedIn Svg */}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        width={size}
+        height={size}
+        viewBox="0 0 48 48"
+      >
+        <path
+          fill={color}
+          d="M8.421 14h.052 0C11.263 14 13 12 13 9.5 12.948 6.945 11.263 5 8.526 5 5.789 5 4 6.945 4 9.5 4 12 5.736 14 8.421 14zM4 17H13V43H4zM44 26.5c0-5.247-4.253-9.5-9.5-9.5-3.053 0-5.762 1.446-7.5 3.684V17h-9v26h9V28h0c0-2.209 1.791-4 4-4s4 1.791 4 4v15h9C44 43 44 27.955 44 26.5z"
+        ></path>
+      </svg>
+    </>
+  );
 };
