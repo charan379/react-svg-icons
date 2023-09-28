3e63f2fe879cec7f5df73cc59b2ca445ea07b2d5
charan379 <37008444+charan379@users.noreply.github.com>
2023-09-28 21:52:12 +0530

    fix attribute naming convention

--- src/components/Communication/Email/EmailSvg.tsx
+++ src/components/Communication/Email/EmailSvg.tsx
@@ -36,18 +36,18 @@ export const EmailSvg: React.FC<EmailSvgProps> = (props) => {
       >
         <g
           fill="none"
-          fill-rule="nonzero"
+          fillRule="nonzero"
           stroke="none"
-          stroke-width="1"
-          stroke-linecap="butt"
-          stroke-linejoin="miter"
-          stroke-miterlimit="10"
-          stroke-dasharray=""
-          stroke-dashoffset="0"
-          font-family="none"
-          font-weight="none"
-          font-size="none"
-          text-anchor="none"
+          strokeWidth="1"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+          strokeMiterlimit="10"
+          strokeDasharray=""
+          strokeDashoffset="0"
+          fontFamily="none"
+          fontWeight="none"
+          fontSize="none"
+          textAnchor="none"
           style={{ mixBlendMode: "normal" }}
         >
           <g transform="scale(5.33333,5.33333)">
